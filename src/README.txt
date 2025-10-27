77d65ce6a29e103367d08bb41c416d0bc7432298
sumaya-sid <sumaya.siddiqui49024@gmail.com>
2025-10-27 13:59:50 -0400

    my work

---
+++ src/README.txt
@@ -0,0 +1,8 @@
+The story begins in a faraway cherry tree filled forest. It was once home to Blossom Village,
+but abandoned due to the Greater-Tiered-War. That war started here, in this village, and you
+are an investigator, part of a team of scientists who are attempting to find out what happened to
+devastate the land, and the World on top of it. Almost everywhere is covered in a dark, black haze,
+except for the old Blossom Village and a few other places. You alone are trying to find out
+the mystery behind these few locations, starting with the Blossom Village and heading on an adventure
+to Partridge Village, CandleWood Camp, Crystal Cottages and Star-Light Islands.
+WARNING: Work will not be saved if you decide to exit during any level.
--- src/main/java/TextAdventure.java
+++ src/main/java/TextAdventure.java
@@ -1,7 +1,48 @@
 import java.util.Scanner;
 
-public class TextAdventure {
+public class TextAdventure
+{
     public static void main(String[] args) {
+        // Scanner class
+        Scanner sc = new Scanner(System.in);
 
+        // Have a boolean value to keep while running
+        boolean run = true;
+
+        while (run) {
+            // Introduce user to the game
+            System.out.println("Welcome to Brookstore Cherry Blossoms!");
+            System.out.println("1. Blossom Village");
+            System.out.println("2. Partridge Village");
+            System.out.println("3. CandleWood Camp");
+            System.out.println("4. Crystal Cottages");
+            System.out.println("5. Star-Light Islands");
+            System.out.println();
+            System.out.print("Please select a level: ");
+            int level = sc.nextInt();
+            System.out.println();
+
+            // Go through each level
+            switch (level) {
+                case 1:
+                    // Helpful side character introduction
+                    System.out.print("Welcome investigator! \nI am Lenny the elf and here to assist you on your adventure. \nPlease enter your name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Nice to meet you " + name + "!");
+
+                    System.out.println("Congratulations! You've found the first piece of the puzzle. \nWould you like to continue on your remarkable journey?: ");
+                    String answer = sc.nextLine();
+                    if (answer == "N")
+                    {
+                        run = false;
+                        System.out.println("Great work investigator! Goodbye!");
+                    }
+
+                    break;
+                default:
+                    // Print an error
+                    System.out.print("Enter a valid number: ");
+            }
+        }
     }
 }
